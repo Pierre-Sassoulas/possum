1c4593b7a41a8735e12ea50889caf0ee53899aa1
2br0ne <code@jbaron.fr>
2014-12-22 16:38:55 +0100

    fix(jukebox): - returning to blank position after selecting a playlist - prevent to send useless requests to the server

--- possum/jukebox/static/jukebox.js
+++ possum/jukebox/static/jukebox.js
@@ -2,36 +2,39 @@ var intervId;
 var intervFin;
 var infos;
 
-String.prototype.toMMSS = function () {
-	var sec_num = parseInt(this, 10);
-	var minutes = Math.floor(sec_num / 60);
-	var seconds = sec_num - (minutes * 60);
+String.prototype.toMMSS = function() {
+  var sec_num = parseInt(this, 10);
+  var minutes = Math.floor(sec_num / 60);
+  var seconds = sec_num - (minutes * 60);
 
-	if (minutes < 10) {minutes = "0"+minutes;}
-	if (seconds < 10) {seconds = "0"+seconds;}
-	var time    = minutes+':'+seconds;
-	return time;
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+  var time = minutes + ':' + seconds;
+  return time;
 }
 
-function setinfos()
-{
-	$.get('info', function( data ) {
-		  infos = $.parseJSON(data);
-		  $("pre#song").html(infos.song);
-		  $("pre#artist").html(infos.artist);
-		  $("pre#time").html(infos.elapsed.toMMSS()+"/"+infos.time.toMMSS());
-		  $( "progress#progressbar" ).attr('value',parseFloat(infos.elapsed)/parseFloat(infos.time)*100);
-		  clearInterval(intervId);
-		  clearInterval(intervFin);
-		  if(infos.status=="play") {
-		  	// refresh at every more 7% played
-		  	intervId = setInterval(setinfos, parseFloat(infos.time)/100*7*1000);
-			// refresh when next song starts to play
-		  	intervFin = setInterval(setinfos, (parseFloat(infos.time)-parseFloat(infos.elapsed)+1)*1000);
-		  	console.log("Refresh every (s) : " + parseFloat(infos.time)/100*7);
-		  	console.log("Next song (s) : " + (parseFloat(infos.time)-parseFloat(infos.elapsed)+1));
-		  }
-		});
+function setinfos() {
+  $.get('info', function(data) {
+    infos = $.parseJSON(data);
+    $("pre#song").html(infos.song);
+    $("pre#artist").html(infos.artist);
+    $("pre#time").html(infos.elapsed.toMMSS() + "/" + infos.time.toMMSS());
+    $("progress#progressbar").attr('value', parseFloat(infos.elapsed) / parseFloat(infos.time) * 100);
+    clearInterval(intervId);
+    clearInterval(intervFin);
+    if (infos.status == "play") {
+      // refresh at every more 7% played
+      intervId = setInterval(setinfos, parseFloat(infos.time) / 100 * 7 * 1000);
+      // refresh when next song starts to play
+      intervFin = setInterval(setinfos, (parseFloat(infos.time) - parseFloat(infos.elapsed) + 1) * 1000);
+      console.log("Refresh every (s) : " + parseFloat(infos.time) / 100 * 7);
+      console.log("Next song (s) : " + (parseFloat(infos.time) - parseFloat(infos.elapsed) + 1));
+    }
+  });
 }
 
 $("button#play").click(ajaxplay);
@@ -40,14 +43,12 @@ $("button#next").click(ajaxnext);
 $("button#previous").click(ajaxprevious);
 $("button#remove").click(ajaxremove);
 
-function ajaxplay()
-{
+function ajaxplay() {
   ajaxfunct('play');
   return false;
 }
 
-function ajaxpause()
-{
+function ajaxpause() {
   ajaxfunct('pause');
   clearInterval(intervId);
   clearInterval(intervFin);
@@ -55,40 +56,39 @@ function ajaxpause()
   return false;
 }
 
-function ajaxfunct(strfunct)
-{
-	$.ajax({
-		url: strfunct,
-		type : 'GET'
-	});
-	setinfos();
-	return false;
+function ajaxfunct(strfunct) {
+  $.ajax({
+    url: strfunct,
+    type: 'GET'
+  });
+  setinfos();
+  return false;
 }
 
-function ajaxnext()
-{
+function ajaxnext() {
   ajaxfunct('next');
 }
 
-function ajaxprevious()
-{
-	ajaxfunct('previous');
+function ajaxprevious() {
+  ajaxfunct('previous');
 }
 
-function ajaxremove()
-{
-	ajaxfunct('remove');
+function ajaxremove() {
+  ajaxfunct('remove');
 }
 
-$(document).ready(function(){
-	setinfos();
-	$("input#subbtn").remove();
-	$("select#id_pl").change(function(){
-		$.ajax({
-		url: 'play',
-		type : 'GET',
-		data : "pl="+$("select#id_pl").val()
-	    });
-	    setinfos();
-	});
+$(document).ready(function() {
+  setinfos();
+  $("input#subbtn").remove();
+  $("select#id_pl").change(function() {
+    if ($("select#id_pl").val() != '0') {
+      $.ajax({
+        url: 'play',
+        type: 'GET',
+        data: "pl=" + $("select#id_pl").val()
+      });
+      $("select#id_pl").val('0');
+    }
+    setinfos();
+  });
 });
